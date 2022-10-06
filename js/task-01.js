b757331cb194de9bb85961437e69c6a60ea6ffb5
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-06 18:47:54 +0300

    1st task finished!

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,23 @@
+const animalsTitle = document.querySelector('.animals__title');
+const animalsItem = document.querySelectorAll('.animals__items');
 
+const productTitle = document.querySelector('.product__title');
+const productItem = document.querySelectorAll('.product__item');
 
+const technologiesTitle = document.querySelector('.technologies__title');
+const technologiesItme = document.querySelectorAll('.technologies__item');
 
+const catagoriesArray = [];
+catagoriesArray.push(animalsTitle, productTitle, technologiesTitle);
+// console.log(catagoriesArray)
 
+console.log(`Number of categories: ${catagoriesArray.length}`);
 
-const testArray = [1, 2, 3]
+console.log(`Categort : ${animalsTitle.textContent}`)
+console.log(`Elements: ${animalsItem.length}`)
 
-console.log(testArray[1])
-const hi = {
-    name: "Vladisval",
-    sename: "Alexeev"
+console.log(`Categort : ${productTitle.textContent}`)
+console.log(`Elements: ${productItem.length}`)
 
-}
-console.log(hi.name)
-
+console.log(`Categort : ${technologiesTitle.textContent}`)
+console.log(`Elements: ${technologiesItme.length}`)
