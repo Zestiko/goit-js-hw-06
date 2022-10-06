1b61efcc61c75e39d449653a098445fb74c2b4ac
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-06 19:25:30 +0300

    try to do second task

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const list = document.createElement("li");
+// console.log(list)
+// list.textContent = ingredients[0];
+// console.log(list.outerHTML)
+
+const makeIngredientsList = ingredients => {
+  return ingredients.map(list => {
+    const listEl = document.createElement("li");
+    listEl.textContent(list);
+    return listEl;
+  });
+};
+makeIngredientsList;
+console.table(makeIngredientsList)
