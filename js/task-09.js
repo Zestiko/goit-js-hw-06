b49fc226a2db4fe792775fe418984614e06b1099
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-09 16:12:02 +0300

    finish task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+};
+
+
+refs.button.addEventListener('click',changeBgcolor);
+
+
+function changeBgcolor(event) {
+  refs.span.textContent = getRandomHexColor();
+  refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+
+}
+
