ee99d40cf22d65ed86765a90393f52c771bd4a2d
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-09 15:41:18 +0300

    Update function

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const refs = {
+    form: document.querySelector('.login-form'),
+};
+// console.log(refs.form)
+refs.form.addEventListener('submit', onFormSubmit);
 
+function onFormSubmit(event) {
+    event.preventDefault();
+    // console.log(event.currentTarget.elements.email.value);
+    // console.log(event.currentTarget.elements.password.value);
+ if (event.currentTarget.elements.email.value === "" || event.currentTarget.elements.password.value === "" ) {
+     alert('Введіть данні')
+    } else {
+     const formData = new FormData(event.currentTarget);
+      formData.forEach((value, name) => {
+        console.log(name);
+        console.log(value);
+    });
+    event.currentTarget.elements.email.value = "";
+    event.currentTarget.elements.password.value = "";
+  }
+    
+    // const formData = new FormData(event.currentTarget);
+    // formData.forEach((value, name) => {
+    //     console.log(name);
+    //     console.log(value);
+    // });
+    // event.currentTarget.elements.email.value = "";
+    // event.currentTarget.elements.password.value = "";
+
+    // console.log(formData);
+}
