f43b484cc136e1a7a8c5fe683407cd09ffa1cdb6
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-10-09 20:19:11 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -7,7 +7,7 @@ console.log(refs.input.dataset.length)
 function chekInputLength(event) {
     refs.input.classList.remove('valid')
     refs.input.classList.remove('invalid')
-    if (event.currentTarget.value.length >= refs.input.dataset.length) {
+    if (event.currentTarget.value.length == refs.input.dataset.length) {
        refs.input.classList.toggle('valid')
     } else {
         refs.input.classList.toggle('invalid')
